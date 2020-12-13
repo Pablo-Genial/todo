012c177af98954ca400af5d2fac2c553fd8a581d
Pablo-Genial <sporthold@yahoo.com>
2020-12-13 21:59:33 +0100

    Some new elemets.

--- todo.js
+++ todo.js
@@ -1,50 +1,61 @@
 // Egy olyan függvény, ami tudja kezelni a localStorage-ot
 (function () {
-// Mock data. Az adatokat dinamikusan kapjuk
-let todos = [];
-// Parts of date
-const bodyDay = document.querySelector('.body__day');
-const bodyDate = document.querySelector('.body__date');
-const todoAddBtn = document.querySelector('.todo__btn');
-const todoInput = document.querySelector('.todo__input');
-const todoListPending = document.querySelector('.todo__list--pending');
+    // Mock data. Az adatokat dinamikusan kapjuk
+    let todos = [];
+    // Parts of date
+    const bodyDay = document.querySelector('.body__day');
+    const bodyDate = document.querySelector('.body__date');
+    const todoAddBtn = document.querySelector('.todo__btn');
+    const todoInput = document.querySelector('.todo__input');
+    const clearAllBtn = document.querySelector('.footer__btn--clear');
+    const todoListPending = document.querySelector('.todo__list--pending');
+    const todoListDone = document.querySelector('.todo__list--done');
+    const tasksPending = document.querySelector('.todo__number');
 
-const dayNames = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-];
+    const dayNames = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+    ];
 
-// LocalStorage handler object
-const localDB = {
-// localDB.setItem('todos', todos);
-    setItem(key, value) {
-        value = JSON.stringify(value);
-        localStorage.setItem(key, value);
-    },
-// localDB.getItem('todos');
-    getItem(key) {
-       const value = localStorage.getItem(key);
-       if (!value) {
-           return null;
-       }
+    // LocalStorage handler object
+    const localDB = {
+        // localDB.setItem('todos', todos);
+        setItem(key, value) {
+            value = JSON.stringify(value);
+            localStorage.setItem(key, value);
+        },
+        // localDB.getItem('todos');
+        getItem(key) {
+            const value = localStorage.getItem(key);
+            if (!value) {
+                return null;
+            }
 
-       return JSON.parse(value);
-    },
-// localDB.removeItem('todos');
-    removeItem(key) {
-       localStorage.removeItem(key);
-    }
-};
+            return JSON.parse(value);
+        },
+        // localDB.removeItem('todos');
+        removeItem(key) {
+            localStorage.removeItem(key);
+        },
+        clearValueArray() {
+            localStorage.clear();
+        },
+        getValueLength(key) {
+            const value = localStorage.getItem(key);
+            const valueArray = JSON.parse(value);
+            return valueArray.length;
+        }
+    };
 
-// Initialize application
+    // Initialize application
     const init = () => {
         const savedTodos = localDB.getItem('todos');
-// ha a savedTodos rendelkezik adatokkal, magyarul nem null
+        // ha a savedTodos rendelkezik adatokkal, magyarul nem null
         if (savedTodos) {
             todos = savedTodos;
         }
@@ -53,25 +64,26 @@ const localDB = {
         setListeners();
     };
 
-
-// Show date.
+    // Show date.
     const showDate = () => {
-            const currentDate = new Date();
-            const day = [
-                currentDate.getMonth() + 1,
-                currentDate.getDate(),
-                currentDate.getFullYear(),
-            ].map( num => num < 10 ? `0${num}` : num );
-  
-            bodyDay.textContent = dayNames[currentDate.getDay()];
-            bodyDate.textContent = day.join('-');
+        const currentDate = new Date();
+        const day = [
+            currentDate.getMonth() + 1,
+            currentDate.getDate(),
+            currentDate.getFullYear(),
+        ].map(num => num < 10 ? `0${num}` : num);
+
+        bodyDay.textContent = dayNames[currentDate.getDay()];
+        bodyDate.textContent = day.join('-');
     };
 
-// Set eventlisteners.
+    // Set eventlisteners.
     const setListeners = () => {
         todoAddBtn.addEventListener('click', addNewTodo);
+        clearAllBtn.addEventListener('click', clearAll)
     };
-// Save and add todo to the database.
+
+    // Save and add todo to the database.
     const addNewTodo = () => {
         const value = todoInput.value;
         if (value === '') {
@@ -87,10 +99,11 @@ const localDB = {
         todos.push(todo);
         localDB.setItem('todos', todos);
         showTodo(todo);
+        tasksPending.innerHTML = localDB.getValueLength('todos');
         todoInput.value = '';
     }
-// Show todo in the list.
 
+    // Show todo in the list.
     const showTodo = todo => {
         const todoItem = document.createElement('div');
         todoListPending.appendChild(todoItem);
@@ -99,28 +112,18 @@ const localDB = {
             <input type="checkbox">
             <span>${todo.text}</span>
             <button class="fa-button">
-                <i class="fa fa-trash"></i>
+                <i class="fa fa-trash""></i>
             </button>
-        `;    
+        `;
     }
 
- 
-
-
-    // Show completed tasks in the list.
-
-    // const showTodo = todo => {
-    //     const todoItem = document.createElement('div');
-    //     todoListPending.appendChild(todoItem);
-
-    //     todoItem.innerHTML = `
-    //         <input type="checkbox">
-    //         <span>${todo.text}</span>
-    //      `;    
-    // }
-
-
-
+    const clearAll = () => {
+        todos = [];
+        localDB.setItem('todos', todos);
+        const todoLength = localDB.getValueLength('todos');
+        tasksPending.innerHTML = todoLength;
+        todoListPending.innerHTML = "";
+    }
 
 
 
